902948b9b41fa8b2bf84d95f14e52b6ddf817927
Bauke Scholtz <balusc@gmail.com>
2026-01-12 05:25:15 -0400

    Fixed `ReferenceError: "mojarra" is not defined` in corner case when mojarra.ael script needs to be rendered in empty page without h:head/h:form/f:ajax

--- impl/src/main/java/com/sun/faces/renderkit/RenderKitUtils.java
+++ impl/src/main/java/com/sun/faces/renderkit/RenderKitUtils.java
@@ -1755,6 +1755,7 @@ public class RenderKitUtils {
             context.getPartialViewContext().getEvalScripts().add(script.toString());
         }
         else {
+            renderFacesJsIfNecessary(context);
             renderScript(context, component, null, script.toString());
         }
     }
